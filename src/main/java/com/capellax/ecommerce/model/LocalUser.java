ac773beff12bff47cd4563b4aa92d29a45394457
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-20 22:01:23 +0300

    create Address and LocalUser entities & reduce boilerplate code using Lombok

---
+++ src/main/java/com/capellax/ecommerce/model/LocalUser.java
@@ -0,0 +1,39 @@
+package com.capellax.ecommerce.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "local_user")
+public class LocalUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "username")
+    private String username;
+
+    @Column(name = "password", nullable = false, length = 1000)
+    private String password;
+
+    @Column(name = "email", nullable = false, unique = true, length = 320)
+    private String email;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.REMOVE, orphanRemoval = true)
+    private List<Address> addresses = new ArrayList<>();
+
+}
