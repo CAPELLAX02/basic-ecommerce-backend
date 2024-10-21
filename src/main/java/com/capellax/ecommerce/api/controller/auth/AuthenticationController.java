64fd9be9acc003cdac635e5d66da92ba522464d1
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-21 13:34:03 +0300

    login service and login controller

--- src/main/java/com/capellax/ecommerce/api/controller/auth/AuthenticationController.java
+++ src/main/java/com/capellax/ecommerce/api/controller/auth/AuthenticationController.java
@@ -1,5 +1,7 @@
 package com.capellax.ecommerce.api.controller.auth;
 
+import com.capellax.ecommerce.api.model.LoginBody;
+import com.capellax.ecommerce.api.model.LoginResponse;
 import com.capellax.ecommerce.api.model.RegistrationBody;
 import com.capellax.ecommerce.exception.UserAlreadyExistsException;
 import com.capellax.ecommerce.service.UserService;
@@ -29,6 +31,19 @@ public class AuthenticationController {
         } catch (UserAlreadyExistsException exp) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<LoginResponse> loginUser(
+            @Valid @RequestBody LoginBody loginBody
+    ) {
+        String jwt = userService.loginUser(loginBody);
+        if (jwt == null) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+        }
+        LoginResponse loginResponse = new LoginResponse();
+        loginResponse.setJwt(jwt);
+        return ResponseEntity.ok(loginResponse);
     }
 
 }
--- src/main/java/com/capellax/ecommerce/service/UserService.java
+++ src/main/java/com/capellax/ecommerce/service/UserService.java
@@ -1,17 +1,23 @@
 package com.capellax.ecommerce.service;
 
+import com.capellax.ecommerce.api.model.LoginBody;
 import com.capellax.ecommerce.api.model.RegistrationBody;
 import com.capellax.ecommerce.exception.UserAlreadyExistsException;
 import com.capellax.ecommerce.model.LocalUser;
 import com.capellax.ecommerce.model.dao.LocalUserDAO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
     private final LocalUserDAO localUserDAO;
+    private final EncryptionService encryptionService; // TODO: create the service
+    private final JWTService jwtService // TODO: create the service
 
     public LocalUser registerUser(
             RegistrationBody registrationBody
@@ -29,5 +35,29 @@ public class UserService {
         user.setPassword(registrationBody.getPassword());
         return localUserDAO.save(user);
     }
+
+    public String loginUser(
+            LoginBody loginBody
+    ) {
+        Optional<LocalUser> optionalUser = localUserDAO.findByUsernameIgnoreCase(loginBody.getUsername());
+        if (optionalUser.isPresent()) {
+            LocalUser user = optionalUser.get();
+
+            if (encryptionService.verifyPassword(loginBody.getPassword()), user.getPassword()) {
+                return jwtService.generateJWT(user);
+            }
+        }
+        return null;
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
