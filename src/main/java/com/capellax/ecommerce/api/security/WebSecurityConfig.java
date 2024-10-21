99300449bb4d42de498cac754ed58dda8d136f44
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-21 21:20:26 +0300

    Security configuration revised

--- src/main/java/com/capellax/ecommerce/api/security/WebSecurityConfig.java
+++ src/main/java/com/capellax/ecommerce/api/security/WebSecurityConfig.java
@@ -1,31 +1,40 @@
 package com.capellax.ecommerce.api.security;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.intercept.AuthorizationFilter;
 
 @Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true)
 public class WebSecurityConfig {
+
+    private final JWTRequestFilter jwtRequestFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(
             HttpSecurity httpSecurity
     ) throws Exception {
-        // TODO: Proper authentication
         return httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(auth -> {
-                    auth.requestMatchers(
-                                    "/auth/**",
-                                    "/orders/**",
-                                    "/products/**"
-                            )
-                            .permitAll()
-                            .anyRequest()
-                            .authenticated();
-                })
+                .cors(Customizer.withDefaults())
+                .authorizeHttpRequests(auth -> auth.requestMatchers(
+                                "/auth/**",
+                                "/orders/**",
+                                "/products/**"
+                        )
+                        .permitAll()
+                        .anyRequest()
+                        .authenticated())
+                .addFilterBefore(jwtRequestFilter, AuthorizationFilter.class)
                 .build();
     }
 
