1043b8e35b66a3e039cca08ebeb0e411e80e9663
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-26 14:39:35 +0300

    JWTServiceTest completed with errors

---
+++ src/test/java/com/capellax/ecommerce/service/JWTServiceTest.java
@@ -0,0 +1,40 @@
+package com.capellax.ecommerce.service;
+
+import com.capellax.ecommerce.model.LocalUser;
+import com.capellax.ecommerce.model.dao.LocalUserDAO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class JWTServiceTest {
+
+    @Autowired
+    private JWTService jwtService;
+
+    @Autowired
+    private LocalUserDAO localUserDAO;
+
+    @Test
+    public void testVerificationTokenNotUsableForLogin() {
+        LocalUser user = localUserDAO.findByUsernameIgnoreCase("UserA").get();
+        String token = jwtService.generateVerificationJWT(user);
+        Assertions.assertNull(
+                jwtService.getUsername(token),
+                "Verification token should not contain username."
+        );
+    }
+
+    @Test
+    public void testAuthTokenReturnsUsername() {
+        LocalUser user = localUserDAO.findByUsernameIgnoreCase("UserA").get();
+        String token = jwtService.generateJWT(user);
+        Assertions.assertEquals(
+                user.getUsername(),
+                jwtService.getUsername(token),
+                "Token for authentication should contain users username."
+        );
+    }
+
+}
