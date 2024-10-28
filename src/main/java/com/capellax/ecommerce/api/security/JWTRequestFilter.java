e672becad7790a6abb12a7474b2daa53d3e15bff
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-28 12:13:35 +0300

    JWT request filter refactored after web socket configuration

--- src/main/java/com/capellax/ecommerce/api/security/JWTRequestFilter.java
+++ src/main/java/com/capellax/ecommerce/api/security/JWTRequestFilter.java
@@ -10,6 +10,10 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
@@ -18,11 +22,15 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
-public class JWTRequestFilter extends OncePerRequestFilter {
+public class JWTRequestFilter
+        extends OncePerRequestFilter
+        implements ChannelInterceptor {
 
     private final JWTService jwtService;
     private final LocalUserDAO localUserDAO;
@@ -34,27 +42,54 @@ public class JWTRequestFilter extends OncePerRequestFilter {
             @NonNull FilterChain filterChain
     ) throws ServletException, IOException {
       String tokenHeader = request.getHeader("Authorization");
-      if (tokenHeader != null && tokenHeader.startsWith("Bearer")) {
-          String token = tokenHeader.substring(7);
-          try {
-              String username = jwtService.getUsername(token);
-              Optional<LocalUser> opUser = localUserDAO.findByUsernameIgnoreCase(username);
-              if (opUser.isPresent()) {
-                  LocalUser user = opUser.get();
-                  if (user.isEmailVerified()) {
-                      UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                              user,
-                              null,
-                              new ArrayList<>()
-                      );
-                      authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                      SecurityContextHolder.getContext().setAuthentication(authentication);
-                  }
-              }
-          } catch (JWTDecodeException exp) {
-              throw new RuntimeException("Invalid JWT token");
-          }
+      UsernamePasswordAuthenticationToken token = checkToken(tokenHeader);
+      if (token != null) {
+          token.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
       }
       filterChain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken checkToken(
+            String token
+    ) {
+        if (token != null && token.startsWith("Bearer ")) {
+            token = token.substring(7);
+            try {
+                String username = jwtService.getUsername(token);
+                Optional<LocalUser> opUser = localUserDAO.findByUsernameIgnoreCase(username);
+                if (opUser.isPresent()) {
+                    LocalUser user = opUser.get();
+                    if (user.isEmailVerified()) {
+                        UsernamePasswordAuthenticationToken authentication =
+                                new UsernamePasswordAuthenticationToken(user, null, new ArrayList());
+                        SecurityContextHolder.getContext().setAuthentication(authentication);
+                        return authentication;
+                    }
+                }
+            } catch (JWTDecodeException exp) {
+                throw new RuntimeException("Invalid JWT token.");
+            }
+        }
+        SecurityContextHolder.getContext().setAuthentication(null);
+        return null;
+    }
+
+    @Override
+    public Message<?> preSend(
+            Message<?> message,
+            MessageChannel channel
+    ) {
+        SimpMessageType messageType = (SimpMessageType) message.getHeaders().get("simpMessageType");
+        if (messageType.equals(SimpMessageType.SUBSCRIBE) || messageType.equals(SimpMessageType.MESSAGE)) {
+            Map nativeHeaders = (Map) message.getHeaders().get("nativeHeaders");
+            if (nativeHeaders != null) {
+                List authTokenList = (List) nativeHeaders.get("Authorization");
+                if (authTokenList != null) {
+                    String tokenHeader = (String) authTokenList.get(0);
+                    checkToken(tokenHeader);
+                }
+            }
+        }
+        return message;
     }
 }
