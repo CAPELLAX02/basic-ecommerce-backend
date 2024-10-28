baa192cc5f0308f99f25ffe047e580120b09d79e
CAPELLAX02 <ahmtatar@gmail.com>
2024-10-28 11:46:00 +0300

    Web socket configuration completed

--- src/main/java/com/capellax/ecommerce/api/security/WebSocketConfiguration.java
+++ src/main/java/com/capellax/ecommerce/api/security/WebSocketConfiguration.java
@@ -1,9 +1,16 @@
 package com.capellax.ecommerce.api.security;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.security.authorization.AuthorizationEventPublisher;
 import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.authorization.SpringAuthorizationEventPublisher;
+import org.springframework.security.messaging.access.intercept.AuthorizationChannelInterceptor;
+import org.springframework.security.messaging.access.intercept.MessageMatcherDelegatingAuthorizationManager;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -12,7 +19,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 @Configuration
 @EnableWebSocket
 @EnableWebSocketMessageBroker
+@RequiredArgsConstructor
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
+    private final ApplicationContext context;
 
     @Override
     public void registerStompEndpoints(
@@ -20,7 +30,7 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
     ) {
         registry.addEndpoint("/websocket")
                 .setAllowedOriginPatterns("**")
-                .withSockJS()
+                .withSockJS();
     }
 
     @Override
@@ -31,6 +41,33 @@ public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer 
         registry.setApplicationDestinationPrefixes("/app");
     }
 
+    private AuthorizationManager<Message<?>> makeMessageAuthorizationManager() {
+        MessageMatcherDelegatingAuthorizationManager.Builder messages =
+                new MessageMatcherDelegatingAuthorizationManager.Builder();
 
+        return messages
+                .simpDestMatchers("/topic/users/**")
+                .authenticated()
+                .anyMessage()
+                .permitAll()
+                .build();
+    }
 
+    @Override
+    public void configureClientInboundChannel(
+            ChannelRegistration registration
+    ) {
+        AuthorizationManager<Message<?>> authorizationManager =
+                makeMessageAuthorizationManager();
+
+        AuthorizationChannelInterceptor authInterceptor =
+                new AuthorizationChannelInterceptor(authorizationManager);
+
+        AuthorizationEventPublisher publisher =
+                new SpringAuthorizationEventPublisher(context);
+
+        authInterceptor.setAuthorizationEventPublisher(publisher);
+
+        registration.interceptors(authInterceptor);
+    }
 }
